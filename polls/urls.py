47e42885adc9b37605f40a2cc4d5fd5a2b5ba436
Lee Sarah <imsolem1226@gmail.com>
2019-08-27 14:32:15 +0900

    add page url patterns to polls/urls.py

--- polls/urls.py
+++ polls/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='index'),    #.../polls/
+    path('<int:question_id>',views.detail, name='detail'),             #.../polls/4
+    path('<int:question_id>/results',views.results, name='results'),     #.../polls/4/result
+    path('<int:question_id>/vote',views.vote, name='vote'),          #.../polls/4/vote
 ]
