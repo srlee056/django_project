63cf807b95c16606b207db9018f40cf49659a5d0
Lee Sarah <imsolem1226@gmail.com>
2019-09-02 18:10:54 +0900

    Code : Add index, detail templates Fix typo

--- polls/models.py
+++ polls/models.py
@@ -3,7 +3,9 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
+
 class Question(models.Model):
+    objects = models.Manager()
     question_text = models.CharField(max_length = 200)
     pub_date = models.DateTimeField('date published')
     def __str__(self):
@@ -12,6 +14,7 @@ class Question(models.Model):
         return self.pub_date >= timezone.now() - datetime.timedelta(days = 1)
 
 class Choice(models.Model):
+    objects = models.Manager()
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
--- polls/urls.py
+++ polls/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),    #.../polls/
-    path('<int:question_id>',views.detail, name='detail'),             #.../polls/4
-    path('<int:question_id>/results',views.results, name='results'),     #.../polls/4/result
-    path('<int:question_id>/vote',views.vote, name='vote'),          #.../polls/4/vote
+    path('<int:question_id>/',views.detail, name='detail'),             #.../polls/4
+    path('<int:question_id>/results/',views.results, name='results'),     #.../polls/4/result
+    path('<int:question_id>/vote/',views.vote, name='vote'),          #.../polls/4/vote
 ]
